3a37da3dc40b29274e1d92b21e5e36f5b036752e
VeronicaGoCa <programandoenlinea@gmail.com>
2026-03-14 17:31:09 -0400

    segunda modificacion

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
+  // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com pr
   // apiUrl: 'https://maneja-tus-gastos.onrender.com',
      apiUrl: 'https://maneja-tus-gastos-gomez.onrender.com',
 };
